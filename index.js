6f9260ced0b240173b0910a9dece6b2d526ab338
theatulanand <thisisatulanand@gmail.com>
2022-05-30 16:58:20 +0530

    enter key functionality added

--- index.js
+++ index.js
@@ -157,3 +157,17 @@ let isValidBoad = function(board) {
     }
     return true
   };
+
+let inputs = document.querySelectorAll("input,select");
+for (var i = 0 ; i < inputs.length; i++) {
+   inputs[i].addEventListener("keypress", function(e){
+      if (e.which == 13) {
+         e.preventDefault();
+         var nextInput = document.querySelectorAll('[tabIndex="' + (this.tabIndex + 1) + '"]');
+         if (nextInput.length === 0) {
+            nextInput = document.querySelectorAll('[tabIndex="1"]');
+         }
+         nextInput[0].focus();
+      }
+   })
+}
